268c1247690c6826a6e32d61c667fc35779e5569
Manish Mathai <manishpmathai@gmail.com>
2025-07-26 03:43:43 -0700

    refactor: migrate WizardProgress to use Mantine Progress component

--- tanaka/extension/src/playground/pages/welcome/components/wizard-progress/wizard-progress.tsx
+++ tanaka/extension/src/playground/pages/welcome/components/wizard-progress/wizard-progress.tsx
@@ -1,30 +1,22 @@
-import "./wizard-progress.scss";
+import { Progress } from "@mantine/core";
 
-import { applyDefaults, type DeepPartial, type WithDefaults } from "@kiku/core";
-import clsx from "clsx";
-
-type WizardProgressPropsBase = {
+export interface WizardProgressProps {
   currentStep: number;
   totalSteps: number;
   className?: string;
-};
+}
 
-const defaultProps = {
-  className: "",
-} as const;
-
-export type WizardProgressProps = WithDefaults<WizardProgressPropsBase, typeof defaultProps>;
-
-export function WizardProgress(props: DeepPartial<WizardProgressProps>) {
-  const { currentStep, totalSteps, className } = applyDefaults(defaultProps, props);
-
+export function WizardProgress({ currentStep, totalSteps, className = "" }: WizardProgressProps) {
   const progressPercentage = ((currentStep - 1) / (totalSteps - 1)) * 100;
 
   return (
-    <div className={clsx("tnk-wizard-progress", className)}>
-      <div className="tnk-wizard-progress__bar">
-        <div className="tnk-wizard-progress__fill" style={{ width: `${progressPercentage}%` }} />
-      </div>
-    </div>
+    <Progress
+      value={progressPercentage}
+      size="xs"
+      radius="xs"
+      className={className}
+      color="indigo"
+      transitionDuration={300}
+    />
   );
 }
